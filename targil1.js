22cfdfd3dda7c92890860deae0cbe9090534849e
זלץ תמר <161@ladaat.local>
2024-11-12 16:51:45 +0200

    sortbyname

--- targil1.js
+++ targil1.js
@@ -1,4 +1,14 @@
 
 for (let i = 0; i < catalog.length; i++) {
     catalog[i]["id"]=i+100
-}         
+}   
+function sortbyname()
+{
+    let st=" "
+    let name=document.getElementById("1").value
+    const arr=catalog.filter(item=>item.title.includes(name))
+    for (let i = 0; i < arr.length; i++) {
+        st+=arr[i].title+" "+arr[i].artist+" "+arr[i].country+" "+arr[i].company+" "+arr[i].price+" "+arr[i].year+"</br>"       
+    }
+    document.getElementById("p1").innerHTML=st
+}      
